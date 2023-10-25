09ca24902cc6426f43450ebe89c95274f7f3859e
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2023-10-25 21:04:30 +0300

    Initial commit

---
+++ src/main/java/com/First_Spring_Project_0323/FirstSpringProject0323Application.java
@@ -0,0 +1,13 @@
+package com.First_Spring_Project_0323;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProject0323Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProject0323Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/First_Spring_Project_0323/FirstSpringProject0323ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.First_Spring_Project_0323;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProject0323ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
